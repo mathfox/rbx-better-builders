0ac06be44350c831aa0131175b59b0d3f09d75ac
nonamie <53796817+mathfox@users.noreply.github.com>
2024-06-01 09:02:47 +0300

    wip

--- src/ExperienceInviteOptionsBuilder.ts
+++ src/ExperienceInviteOptionsBuilder.ts
@@ -1,1 +1,52 @@
-export class ExperienceInviteOptionsBuilder {}
+export class ExperienceInviteOptionsBuilder {
+	private promptMessage?: string;
+	private launchData?: string;
+	private inviteUser?: number;
+	private inviteMessageId?: string;
+
+	setPromptMessage(promptMessage: string) {
+		this.promptMessage = promptMessage;
+
+		return this;
+	}
+
+	setLaunchData(launchData: string) {
+		this.launchData = launchData;
+
+		return this;
+	}
+
+	setInviteUser(inviteUser: number) {
+		this.inviteUser = inviteUser;
+
+		return this;
+	}
+
+	setInviteMessageId(inviteMessageId: string) {
+		this.inviteMessageId = inviteMessageId;
+
+		return this;
+	}
+
+	build() {
+		const options = new Instance("ExperienceInviteOptions");
+
+		if (this.promptMessage !== undefined) {
+			options.PromptMessage = this.promptMessage;
+		}
+
+		if (this.launchData !== undefined) {
+			options.LaunchData = this.launchData;
+		}
+
+		if (this.inviteUser !== undefined) {
+			options.InviteUser = this.inviteUser;
+		}
+
+		if (this.inviteMessageId !== undefined) {
+			options.InviteMessageId = this.inviteMessageId;
+		}
+
+		return options;
+	}
+}
--- src/TweenInfoBuilder.ts
+++ src/TweenInfoBuilder.ts
@@ -1,44 +1,48 @@
 export class TweenInfoBuilder {
-	public easingDirection: Enum.EasingDirection = Enum.EasingDirection.Out;
-	public easingStyle: Enum.EasingStyle = Enum.EasingStyle.Quad;
-	public time = 1;
-	public repeatCount = 0;
-	public reverses = false;
-	public delayTime = 0;
+	private time = 1;
+	private easingStyle: Enum.EasingStyle = Enum.EasingStyle.Quad;
+	private easingDirection: Enum.EasingDirection = Enum.EasingDirection.Out;
+	private repeatCount = 0;
+	private reverses = false;
+	private delayTime = 0;
 
-	public SetEasingDirection(easingDirection: Enum.EasingDirection) {
-		this.easingDirection = easingDirection;
-
-		return this;
-	}
-
-	public SetTime(time: number) {
+	setTime(time: number) {
 		this.time = time;
 
 		return this;
 	}
 
-	public SetDelayTime(delayTime: number) {
-		this.delayTime = delayTime;
-		return this;
-	}
-
-	public SetReverses(reverses: boolean) {
-		this.reverses = reverses;
-		return this;
-	}
-
-	public SetRepeatCount(repeatCount: number) {
-		this.repeatCount = repeatCount;
-		return this;
-	}
-
-	public SetEasingStyle(easingStyle: Enum.EasingStyle) {
+	setEasingStyle(easingStyle: Enum.EasingStyle) {
 		this.easingStyle = easingStyle;
+
 		return this;
 	}
 
-	public Build() {
+	setEasingDirection(easingDirection: Enum.EasingDirection) {
+		this.easingDirection = easingDirection;
+
+		return this;
+	}
+
+	setRepeatCount(repeatCount: number) {
+		this.repeatCount = repeatCount;
+
+		return this;
+	}
+
+	setReverses(reverses: boolean) {
+		this.reverses = reverses;
+
+		return this;
+	}
+
+	setDelayTime(delayTime: number) {
+		this.delayTime = delayTime;
+
+		return this;
+	}
+
+	build() {
 		return new TweenInfo(
 			this.time,
 			this.easingStyle,
